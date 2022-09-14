e510af55da97961d126992cf1acb1bb2a0d55b52
Stephaney <80514810+changs14@users.noreply.github.com>
2022-09-14 10:01:48 -0700

    Add files via upload

--- CS302/Assignment 2/main.cpp
+++ CS302/Assignment 2/main.cpp
@@ -8,16 +8,18 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-int findEmptyBox(char);
-int findNextBox(char, int, int);
-bool solveCrossword(char);
+vector<string> enterRows(char[10][10], int, int, string);
+void enterColumn(char[10][10], int, int, string);
+bool emptyCrossWord(char [10][10]);
+bool solveCrossword(char [10][10], vector<string>);
 
 
 int main(){
-    char crossword[10][10]; //Crossword board
+    vector<string> crossword; //Crossword board
     vector<string> words;
 
     //File variables
@@ -26,7 +28,6 @@ int main(){
     ifstream inFile;
     
     //Get user input file
-
     do{
         cout<<"Enter file name: ";
         cin>>filename;
@@ -45,15 +46,100 @@ int main(){
     
     //Read in file, assign to variables as needed
     //First ten lines will be assigned to the crossword variable
+    for (int i = 0; i < 10; i++){
+        inFile>>line;
+        crossword.push_back(line);
+    }
+
+    //The the words in the list
+    inFile.ignore(1000, '\n');  //Ignore the space between grid and words
+
+    //Get the words
+    for(int i = 0; i<4; i++){
+        inFile>>line;   //Get current word in the line
+        words.push_back(line); //Add to the words list
+    }
 
     //Close the file
     inFile.close();
 
     //Call functions
-
+    solveCrossword(crossword, words);
+    
     //Print the solved crossword puzzle
 
+    for(int i=0; i<10;i++){
+        cout<<crossword[i]<<endl;
+    }
 
   return 0;
   
+}
+
+ enterRows(char board[10][10], int row, int column, string currWord){
+    //Find a free position of the cross word represented by '-'
+    for(int i=0; i<currWord.length();i++){
+        if(board[row+i][column] == '-' || board[row+i][column] == currWord[i]){
+            board[row+i][column] = currWord[i];
+        }
+        else{
+            board[0][0] = 'x';
+        }
+    }
+
+}
+
+void enterColumn(char board[10][10], int row, int column, string currWord){
+    //Find a free position in the columns represented by '-'
+    for(int i=0; i<currWord.length();i++){
+        if(board[row][column+i] == '-' || board[row+i][column] == currWord[i]){
+            board[row][column+i] = currWord[i];
+        }
+        else{
+            board[0][0] = 'x';
+        }
+
+    }   
+}
+
+bool emptyCrossWord(char board[10][10]){
+    for(int i=0; i<10;i++){
+        for(int j =0; j<10;j++){
+            if(board[i][j] == '-'){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool solveCrossword(char board[10][10], vector<string> wordList){
+    //Solve the crossword puzzle using the backtracking algorithm and recursion
+
+    string current;
+    bool empty = emptyCrossWord(board);
+
+    if (empty == false){
+        return true;
+    }
+
+    for(int k=0; k<4;k++){
+        current = wordList[k];
+        
+        for(int i=0; i<10;i++){
+            //Check the vertical rows
+            for(int j=0;j<current.length();j++){
+                enterRows(board, j, i, current);
+                enterColumn(board, i, j, current);
+
+                if (board[0][0] != 'x'){
+                        solveCrossword(board, wordList);
+                }
+
+            }
+        }
+            
+    }
+        return false;
+    
 }
