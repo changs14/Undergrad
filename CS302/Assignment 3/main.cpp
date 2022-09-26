eda0885d550de2e998812a8938ac2e5835b373a4
Stephaney <80514810+changs14@users.noreply.github.com>
2022-09-26 01:35:34 -0700

    Update main.cpp

--- CS302/Assignment 3/main.cpp
+++ CS302/Assignment 3/main.cpp
@@ -1,3 +1,10 @@
+/* Name: Stephaney Chang
+   NSHEID: 2001508920
+   Class: CS 302
+   Assignment: 3
+   Description: This program will deal with matrices, linear algebra, and threads
+*/
+
 #include <iostream>
 #include <thread>
 
