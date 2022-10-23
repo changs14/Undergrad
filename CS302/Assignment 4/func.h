2743a5d0fb71e9e30fd8e855d7b9f3121f3c5dbf
Stephaney <80514810+changs14@users.noreply.github.com>
2022-10-23 09:21:38 -0700

    Update func.h

--- CS302/Assignment 4/func.h
+++ CS302/Assignment 4/func.h
@@ -8,79 +8,90 @@
 #include <iostream>
 #include <string>
 
-class binTree
-{
-    struct binTreeNode
-    {
-        char letter;
-        int frequency;
-        binTreeNode * left, * right;
-    };
+class binTree {
+  struct binTreeNode {
+    char letter;
+    int frequency;
+    binTreeNode *left, *right;
+  };
 
-    public:
-        //Construtors
-        binTree();
-        binTree(char, int);
-        binTree(binTree*, binTree*);
-        ~binTree();
-        
-        //Misc functions
-        int getFrequency() const;
-        std::string getPrefixCode(char);
+public:
+  // Construtors
+  binTree();
+  binTree(char, int);
+  binTree(binTree *, binTree *);
+  ~binTree();
 
-    private:
-        std::string getPrefixCode(binTreeNode*, char);
-        void destroyTree(binTreeNode*);
+  // Misc functions
+  int getFrequency() const;
+  std::string getPrefixCode(char);
 
-        //Variables
-        binTreeNode * root;
+private:
+  std::string getPrefixCode(binTreeNode *, char);
+  void destroyTree(binTreeNode *);
+
+  // Variables
+  binTreeNode *root;
 };
 
-//CONSTRUCTORS
-binTree::binTree()
-{
-    root = nullptr;
+// CONSTRUCTORS
+binTree::binTree() { root = nullptr; }
+
+binTree::binTree(char letter, int frequency) {
+  root = new binTreeNode;
+  root->letter = letter;
+  root->frequency = frequency;
+
+  // Set children to nulls
+  root->left = nullptr;
+  root->right = nullptr;
 }
 
-binTree::binTree(char letter, int frequency)
-{
-    root = new binTreeNode;
-    root.letter = letter;
-    root.frequency = frequency;
-
-    //Set children pointers to nullptr
+binTree::binTree(binTree *t1, binTree *t2) {
+  // Constructor combines two trees together
+  root = new binTreeNode;
+  root->letter = '\0';
+  root->left = t1->root;
+  root->right = t2->root;
+  root->frequency = root->left->frequency + root->right->frequency;
 }
 
-binTree::binTree(binTree* t1; binTree* t2)
-{
-    //Constructor combines two trees together
-    root = new binTreeNode;
-    root.letter = '\0';
-    root->left = t1.root;
-    root->right = t2.root;
-    root.frequency = t1.frequency + t2.frequency;
+binTree::~binTree() { destroyTree(root); }
+
+void binTree::destroyTree(binTreeNode *r) {
+  // REcursive deallocate post order
+
+  // Check if tree is empty
+  if (r == nullptr) {
+    return;
+  }
+
+  destroyTree(r->left);
+  destroyTree(r->right);
+
+  delete r;
 }
 
-binTree::~binTree(){
-    destroyTree(root);
+int binTree::getFrequency() const { return root->frequency; }
+
+std::string binTree::getPrefixCode(char letter) {
+  std::string code = binTree::getPrefixCode(root, letter);
+
+  return code;
+  
 }
 
-void binTree::destroyTree(binTreeNode* r)
-{
-    //REcursive deallocate post order
-}
+std::string binTree::getPrefixCode(binTreeNode *r, char letter) {
+  std::string code;
+  
+  if (r==nullptr){
+    return nullptr;
+  }
 
-void binTree::getFrequency() const
-{
-    return root.frequency;
-}
+  if(r->letter == letter){
+    code = letter;
+    return code;
+  }
 
-std::string binTree::getPrefixCode(char letter)
-{
-
-}
-
-std::string binTree::getPrefixCode(binTreeNode *r, char letter)
-{
-
+  return binTree::getPrefixCode(r, letter);
 }
